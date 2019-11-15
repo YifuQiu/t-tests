d6913ea044beb64ec44a2d0ed04e0b435764a7cf
Mike Freeman <michael.k.freeman@gmail.com>
2019-11-15 21:49:10 +0100

    Remove answers

--- birth_weights.R
+++ birth_weights.R
@@ -1,51 +1,31 @@
 # Motivaing question:
 # Is there an observable difference in baby weight for smokers / non-smokers?
 
-# Install/library the `openintro package
+# Install/library the `openintro` package
 # install.packages('openintro')
-library(openintro)
-library(dplyr)
-library(ggplot2)
+# You'll also want to load dplyr and ggplot2 (as usual)
 
 # You should now be able to `View` the `births` dataset
 
 # Make an overlapping histogram of the birth weights of smokers/non-smokers
 # This is assessing the univariate distribution by a categorical variable
-ggplot(births) +
-  geom_histogram(mapping = aes(x = weight, fill=smoke))
 
 # What are the mean birth weights for smokers/non-smokers
-by_smoker <- births %>% 
-  group_by(smoke) %>% 
-  summarize(mean_weight = mean(weight))
 
-# Is this difference statistically significant?
+# Is this difference statistically significant (to be answered via steps below)?
 
 # Pre-t-test conceptaul question: is this data paired?
 
 # Store weights from smokers/non-smokers in separate variables for easier access
-smokers <- births %>% filter(smoke == "smoker") %>% select(weight)
-non_smokers <- births %>% filter(smoke == "nonsmoker") %>% select(weight)
-
 
 # Implement a t-test to assess the significance of this difference
-t.test(smokers, non_smokers)
 
-# Now, let's calculate the t value and confidence intervals manually
+# Bonus: let's calculate the t value and confidence intervals manually
 
 # Compute the difference in means
-diff_means <- mean(smokers$weight) - mean(non_smokers$weight)
 
 # Calculate the standard error
-se <- sqrt(
-  (var(smokers$weight)/length(smokers$weight)) +
-    (var(non_smokers$weight)/length(non_smokers$weight))
-)
 
 # Compute the t score
-t_score <- diff_means/se
 
 # Compute CIs using t score (you'll need to reference a t table)
-t <- 2.009 # from t table
-ci_lower <- diff_means - t * se
-ci_upper <- diff_means + t * se
